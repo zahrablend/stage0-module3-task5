d937758b00678bda339e771d222da6ef35e9f0ba
zahrablend <zahra@LAPTOP-010BK67M>
2023-09-20 11:56:21 +0300

    Implement stage0-module3-task5

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,8 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(int toBeDivided) {
+    void divide() {
+        float number = (float) 19 / 5;
+        System.out.println(number);
     }
 }
